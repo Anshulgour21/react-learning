7298762a4bbd4a8de5e0fb23d68f0081c8243b28
Anshul Gour <anshulgaur7999@gmail.com>
2026-04-21 22:45:28 +0530

    Added CSS folder

---
+++ 05-Css/src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Button.module.css'
+ const Button = () => {
+  return (
+    <div>
+        <h1>hello mr.anshul</h1>
+        <h2>its an great opertunties for you</h2>
+        <button>click here</button>
+    </div>
+  )
+}
+export default Button 
